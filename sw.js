cf68187e25825505b277fd65d6a55c91e00e0b56
elmer jurado <elmerjs@gmailcom>
2026-05-15 09:52:03 -0500

    PWA inicial

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'pgn-study-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // NO agregues audios aquí si son muy pesados
+];
+
+// Instalar el service worker y cachear archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Interceptar peticiones y servir desde cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Limpiar cache viejo
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
