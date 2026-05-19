b6a8af6d9df0431194aa9cc929e8eadf228d0a6d
elmer jurado <elmerjs@gmailcom>
2026-05-19 07:53:46 -0500

    verificacion install

--- sw.js
+++ sw.js
@@ -3,35 +3,32 @@ const urlsToCache = [
   '/pgn-study/',
   '/pgn-study/index.html',
   '/pgn-study/styles.css',
-  '/pgn-study/script.js',
+  '/pgn-study/script3.js',
   '/pgn-study/manifest.json',
   '/pgn-study/icon-192.png',
   '/pgn-study/icon-512.png'
   // NO agregues audios aquí si son muy pesados
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
+// Estrategia de Fetch estándar para PWAs
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
 
+// Activación y limpieza
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Eliminando caché antiguo:', cacheName);
             return caches.delete(cacheName);
           }
         })
-      )
-    )
+      );
+    })
   );
 });
